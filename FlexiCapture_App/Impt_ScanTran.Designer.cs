6a0d0822bd40ad3dc550419b9179c9bfb1224fe2
PC-23 <PC-23@ETSI-MARKS>
2018-03-01 12:48:56 +0800

    Remove inserting table name and Add Automatic viewing of imported data.

--- FlexiCapture_App/Impt_ScanTran.Designer.cs
+++ FlexiCapture_App/Impt_ScanTran.Designer.cs
@@ -29,19 +29,18 @@
         private void InitializeComponent()
         {
             this.btn_browse = new System.Windows.Forms.Button();
-            this.tb_table_name = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.btn_ok = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.tb_data_source = new System.Windows.Forms.TextBox();
             this.openfile_browse = new System.Windows.Forms.OpenFileDialog();
             this.dg_data_imported = new System.Windows.Forms.DataGridView();
+            this.chk_view_imp_data = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.dg_data_imported)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_browse
             // 
-            this.btn_browse.Location = new System.Drawing.Point(418, 73);
+            this.btn_browse.Location = new System.Drawing.Point(344, 49);
             this.btn_browse.Name = "btn_browse";
             this.btn_browse.Size = new System.Drawing.Size(83, 20);
             this.btn_browse.TabIndex = 32;
@@ -49,26 +48,9 @@
             this.btn_browse.UseVisualStyleBackColor = true;
             this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
             // 
-            // tb_table_name
-            // 
-            this.tb_table_name.Location = new System.Drawing.Point(184, 112);
-            this.tb_table_name.Name = "tb_table_name";
-            this.tb_table_name.Size = new System.Drawing.Size(228, 20);
-            this.tb_table_name.TabIndex = 31;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.SystemColors.Control;
-            this.label6.Location = new System.Drawing.Point(111, 112);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(66, 13);
-            this.label6.TabIndex = 30;
-            this.label6.Text = "Table name:";
-            // 
             // btn_ok
             // 
-            this.btn_ok.Location = new System.Drawing.Point(257, 156);
+            this.btn_ok.Location = new System.Drawing.Point(183, 122);
             this.btn_ok.Name = "btn_ok";
             this.btn_ok.Size = new System.Drawing.Size(75, 23);
             this.btn_ok.TabIndex = 29;
@@ -79,7 +61,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(111, 48);
+            this.label2.Location = new System.Drawing.Point(37, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(244, 13);
             this.label2.TabIndex = 28;
@@ -87,7 +69,7 @@
             // 
             // tb_data_source
             // 
-            this.tb_data_source.Location = new System.Drawing.Point(183, 73);
+            this.tb_data_source.Location = new System.Drawing.Point(109, 49);
             this.tb_data_source.Name = "tb_data_source";
             this.tb_data_source.Size = new System.Drawing.Size(229, 20);
             this.tb_data_source.TabIndex = 27;
@@ -104,15 +86,24 @@
             this.dg_data_imported.Size = new System.Drawing.Size(206, 150);
             this.dg_data_imported.TabIndex = 33;
             // 
+            // chk_view_imp_data
+            // 
+            this.chk_view_imp_data.AutoSize = true;
+            this.chk_view_imp_data.Location = new System.Drawing.Point(109, 86);
+            this.chk_view_imp_data.Name = "chk_view_imp_data";
+            this.chk_view_imp_data.Size = new System.Drawing.Size(164, 17);
+            this.chk_view_imp_data.TabIndex = 34;
+            this.chk_view_imp_data.Text = "Check to view imported data.";
+            this.chk_view_imp_data.UseVisualStyleBackColor = true;
+            // 
             // Impt_ScanTran
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 190);
+            this.ClientSize = new System.Drawing.Size(460, 170);
+            this.Controls.Add(this.chk_view_imp_data);
             this.Controls.Add(this.dg_data_imported);
             this.Controls.Add(this.btn_browse);
-            this.Controls.Add(this.tb_table_name);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.btn_ok);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.tb_data_source);
@@ -128,12 +119,11 @@
         #endregion
 
         private System.Windows.Forms.Button btn_browse;
-        private System.Windows.Forms.TextBox tb_table_name;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button btn_ok;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox tb_data_source;
         private System.Windows.Forms.OpenFileDialog openfile_browse;
         private System.Windows.Forms.DataGridView dg_data_imported;
+        private System.Windows.Forms.CheckBox chk_view_imp_data;
     }
 }
