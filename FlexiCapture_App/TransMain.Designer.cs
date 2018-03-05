5e0f3051b13834b7f0563a7920049ff0ba56cbe0
etsideb <etsideb@gmail.com>
2018-03-05 14:37:26 +0800

    Added Summary View

--- FlexiCapture_App/TransMain.Designer.cs
+++ FlexiCapture_App/TransMain.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TransMain));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.initializeDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.transactionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -39,12 +40,12 @@
             this.unmatchedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.archivedTransactionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.actionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.scannedFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.iCBSFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.matchingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.archiveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.initializeDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setUpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.matchingCriteriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,6 +54,7 @@
             this.menuStrip1.BackColor = System.Drawing.SystemColors.HighlightText;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStrip,
+            this.setUpToolStripMenuItem,
             this.viewToolStripMenuItem,
             this.actionsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
@@ -69,6 +71,13 @@
             this.fileToolStrip.Name = "fileToolStrip";
             this.fileToolStrip.Size = new System.Drawing.Size(37, 20);
             this.fileToolStrip.Text = "File";
+            // 
+            // initializeDatabaseToolStripMenuItem
+            // 
+            this.initializeDatabaseToolStripMenuItem.Name = "initializeDatabaseToolStripMenuItem";
+            this.initializeDatabaseToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.initializeDatabaseToolStripMenuItem.Text = "Initialize Database";
+            this.initializeDatabaseToolStripMenuItem.Click += new System.EventHandler(this.initializeDatabaseToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
@@ -93,76 +102,44 @@
             this.matchedToolStripMenuItem,
             this.unmatchedToolStripMenuItem});
             this.transactionsToolStripMenuItem.Name = "transactionsToolStripMenuItem";
-            this.transactionsToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.transactionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.transactionsToolStripMenuItem.Text = "Transactions ";
             // 
             // importedToolStripMenuItem
             // 
             this.importedToolStripMenuItem.Name = "importedToolStripMenuItem";
-            this.importedToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.importedToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.importedToolStripMenuItem.Text = "Imported";
             this.importedToolStripMenuItem.Click += new System.EventHandler(this.importedToolStripMenuItem_Click);
             // 
             // matchedToolStripMenuItem
             // 
             this.matchedToolStripMenuItem.Name = "matchedToolStripMenuItem";
-            this.matchedToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.matchedToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.matchedToolStripMenuItem.Text = "Matched";
             this.matchedToolStripMenuItem.Click += new System.EventHandler(this.matchedToolStripMenuItem_Click);
             // 
             // unmatchedToolStripMenuItem
             // 
             this.unmatchedToolStripMenuItem.Name = "unmatchedToolStripMenuItem";
-            this.unmatchedToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.unmatchedToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.unmatchedToolStripMenuItem.Text = "Unmatched";
             this.unmatchedToolStripMenuItem.Click += new System.EventHandler(this.unmatchedToolStripMenuItem_Click);
             // 
             // archivedTransactionsToolStripMenuItem
             // 
             this.archivedTransactionsToolStripMenuItem.Name = "archivedTransactionsToolStripMenuItem";
-            this.archivedTransactionsToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.archivedTransactionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.archivedTransactionsToolStripMenuItem.Text = "Archived";
             this.archivedTransactionsToolStripMenuItem.Click += new System.EventHandler(this.archivedTransactionsToolStripMenuItem_Click);
             // 
             // actionsToolStripMenuItem
             // 
             this.actionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.importToolStripMenuItem,
-            this.matchingToolStripMenuItem,
             this.archiveToolStripMenuItem});
             this.actionsToolStripMenuItem.Name = "actionsToolStripMenuItem";
             this.actionsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
             this.actionsToolStripMenuItem.Text = "Actions";
-            // 
-            // importToolStripMenuItem
-            // 
-            this.importToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.scannedFilesToolStripMenuItem,
-            this.iCBSFileToolStripMenuItem});
-            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
-            this.importToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.importToolStripMenuItem.Text = "Import";
-            // 
-            // scannedFilesToolStripMenuItem
-            // 
-            this.scannedFilesToolStripMenuItem.Name = "scannedFilesToolStripMenuItem";
-            this.scannedFilesToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
-            this.scannedFilesToolStripMenuItem.Text = "Scanned Transactions";
-            this.scannedFilesToolStripMenuItem.Click += new System.EventHandler(this.scannedFilesToolStripMenuItem_Click);
-            // 
-            // iCBSFileToolStripMenuItem
-            // 
-            this.iCBSFileToolStripMenuItem.Name = "iCBSFileToolStripMenuItem";
-            this.iCBSFileToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
-            this.iCBSFileToolStripMenuItem.Text = "ICBS Transactions";
-            this.iCBSFileToolStripMenuItem.Click += new System.EventHandler(this.iCBSFileToolStripMenuItem_Click);
-            // 
-            // matchingToolStripMenuItem
-            // 
-            this.matchingToolStripMenuItem.Name = "matchingToolStripMenuItem";
-            this.matchingToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.matchingToolStripMenuItem.Text = "Matching";
-            this.matchingToolStripMenuItem.Click += new System.EventHandler(this.matchingToolStripMenuItem_Click);
             // 
             // archiveToolStripMenuItem
             // 
@@ -171,12 +148,44 @@
             this.archiveToolStripMenuItem.Text = "Archive";
             this.archiveToolStripMenuItem.Click += new System.EventHandler(this.archiveToolStripMenuItem_Click);
             // 
-            // initializeDatabaseToolStripMenuItem
+            // setUpToolStripMenuItem
             // 
-            this.initializeDatabaseToolStripMenuItem.Name = "initializeDatabaseToolStripMenuItem";
-            this.initializeDatabaseToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
-            this.initializeDatabaseToolStripMenuItem.Text = "Initialize Database";
-            this.initializeDatabaseToolStripMenuItem.Click += new System.EventHandler(this.initializeDatabaseToolStripMenuItem_Click);
+            this.setUpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importSettingsToolStripMenuItem,
+            this.matchingCriteriaToolStripMenuItem});
+            this.setUpToolStripMenuItem.Name = "setUpToolStripMenuItem";
+            this.setUpToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.setUpToolStripMenuItem.Text = "SetUp";
+            // 
+            // importSettingsToolStripMenuItem
+            // 
+            this.importSettingsToolStripMenuItem.Name = "importSettingsToolStripMenuItem";
+            this.importSettingsToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.importSettingsToolStripMenuItem.Text = "Import Settings";
+            // 
+            // matchingCriteriaToolStripMenuItem
+            // 
+            this.matchingCriteriaToolStripMenuItem.Name = "matchingCriteriaToolStripMenuItem";
+            this.matchingCriteriaToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.matchingCriteriaToolStripMenuItem.Text = "Matching Criteria";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(51, 80);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(429, 398);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(513, 80);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(429, 398);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
             // 
             // TransMain
             // 
@@ -184,6 +193,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(1362, 696);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
@@ -204,10 +215,6 @@
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem actionsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem scannedFilesToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem iCBSFileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem matchingToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem archiveToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem archivedTransactionsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem transactionsToolStripMenuItem;
@@ -215,6 +222,11 @@
         private System.Windows.Forms.ToolStripMenuItem matchedToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem unmatchedToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem initializeDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setUpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem matchingCriteriaToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
